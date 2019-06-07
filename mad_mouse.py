79b066237a4e7bb16ddd804f8f861b42ec3f9e32
Mordulv <50501108+Mordulv@users.noreply.github.com>
2019-06-07 23:25:03 +0200

    Add files via upload

---
+++ mad_mouse.py
@@ -0,0 +1,8 @@
+from pynput.mouse import Controller, Button
+from time import sleep
+from random import randint as al
+
+raton = Controller()
+
+while True:
+    raton.position = (al(-1000,1000),al(-1000,1000))
